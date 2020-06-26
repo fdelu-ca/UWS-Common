a039b624c78bce8f79886cf07905c275ac8787d7
De Luca, Fernando <fernando.deluka@arcelormittal.com>
2020-06-26 11:16:34 +0300

    -- Add WaitingJoinUZ

--- Stage.cs
+++ Stage.cs
@@ -3,21 +3,22 @@
     public enum Stage : int
     {
         //Main Stage
-        Weighed       = 0x10,        //Взвешен
-        SendToUWS     = 0x20,        //Передан на Sql сервер
-        MachiningOver = 0x30,        //Машинная обработка завершена
-        Complited     = 0x40,        //Завершена
-        Archived      = 0x50,        //Архивированна 
+        Weighed             = 0x10,        //Взвешен
+        SendToUWS           = 0x20,        //Передан на Sql сервер
+        MachiningOver       = 0x30,        //Машинная обработка завершена
+        Complited           = 0x40,        //Завершена
+        Archived            = 0x50,        //Архивированна 
         
         //Sub Stage
-        SendToFtp     = 0x21,        //Передан на Ftp сервер
-        Recognized    = 0x24,        //Распознан
-        UzJoined      = 0x25,        //Распознан
-        Checked       = 0x35,        //Данные проверены оператором
-        SendToSAP     = 0x47,        //Отправлен в SAP
+        SendToFtp           = 0x21,        //Передан на Ftp сервер
+        Recognized          = 0x24,        //Распознан
+        WaitingJoinUZ       = 0x32,        //Обновление вагонов с сервера UZ
+        Checked             = 0x35,        //Данные проверены оператором
+        WaitDocFormating    = 0x37,        //Ожидание формирования
+        SendToSAP           = 0x47,        //Отправлен в SAP
 
         //Common
-        Undefined     = 0x00,        //Не имеет статуса 
-        Defect        = 0xFF,        //Забраковано
+        Undefined           = 0x00,        //Не имеет статуса 
+        Defect              = 0xFF,        //Забраковано
     }
 }
